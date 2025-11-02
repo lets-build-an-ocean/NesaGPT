6fbe6e3cb6a8975fcfb90519f86691d614829f23
Xtreme <sahebesabr11228@gmail.com>
2025-11-02 20:38:09 +0330

    refactored userauth/views

--- backend/userauth/views.py
+++ backend/userauth/views.py
@@ -1,26 +1,16 @@
-from django.shortcuts import render
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from aichatbot import serializers
-from . import models
+
 from aichatbot.models import Profile, Balance
+
+from aichatbot import serializers
 
 
 class whoami(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
-
-    # def post(self, request):
-    #     this_user = request.user
-    #     wallet_update = request.POST.get("amount")
-    #     this_text = request.POST.get("text")
-    #     this_balance = models.Balance.objects.get(user=this_user)
-    #     this_balance.wallet = wallet_update
-    #     this_balance.save()
-    #     return Response({"wallet": this_balance.wallet})
 
     def get(self, request):
         this_user = request.user
